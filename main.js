9515d215c8900bda8a94b6058b0a1f0e450cb605
Irineo Ramos <irineo.ramos@codeimmersives.com>
2021-03-11 13:08:51 -0600

    all pass.

--- main.js
+++ main.js
@@ -1,6 +1,51 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(user) {
+   return user.userRole === 'ADMIN';
+}
+/*
+Function isAdminAlternate(user) {
+    if ( user.userRole === 'ADMIN') {
+        return true;
+    } else {
+        return false;
+    }
+}
+*/
+
+function getEmail(user){
+    const lowerFirst = user.firstName.toLowerCase();
+    const lowerLast = use.lastName.toLowerCase();
+
+    return `${lowerFirst} ${lowerLast}@codeimmersives.com`;
+}
+
+function getPlaylistLength(playlistObject){
+    return playlistObject.songs.length;
+}
+
+function getHardestHomework(homeworks) {
+    let smallest = 100;
+    let nameOfHardest = '';
+
+     for (const item of homeworks) {
+         if ( item.averageScore < smallest ) {
+             nameOfHardest = item.name;
+             smallest = item.averageScore;
+         }
+     }
+     return nameOfHardest;
+}
+
+function createPhonebook(names, numbers) {
+    const phonebook = [];
+
+    for ( let i = 0; i < names.length; i++ ) {
+        phonebook[names[i]] = numbers[i];
+    }
+    return phonebook;
+}
 
 
 
